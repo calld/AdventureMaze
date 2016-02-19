1756c6850c099eaf9dcb79157bd96151dadd3ede
Devon Call <calld2@quest-OptiPlex-9020>
2016-02-19 10:20:28 -0800

    added SudoLocation.java

--- src/ways/structure/save/SudoContainer.java
+++ src/ways/structure/save/SudoContainer.java
@@ -10,11 +10,11 @@ import ways.structure.Element;
 import java.util.List;
 
 public abstract class SudoContainer extends SudoElement implements Container{
-   
+
    public SudoContainer(String name){
       super(name);
    }
-   
+
    private Container getActual() throws UnsupportedOperationException{
       Element temp = lookup();
       Container c = null;
@@ -25,35 +25,35 @@ public abstract class SudoContainer extends SudoElement implements Container{
       }
       return c;
    }
-   
+
    public boolean addItem(Item item) throws UnsupportedOperationException{
       return getActual().addItem(item);
    }
-   
+
    public boolean addItem(Item item, int number) throws UnsupportedOperationException{
       return getActual().addItem(item, number);
    }
-   
+
    public List<Item> getContents() throws UnsupportedOperationException{
       return getActual().getContents();
    }
-   
+
    public boolean removeItem(Item item) throws UnsupportedOperationException{
       return getActual().removeItem(item);
    }
-   
+
    public double getSizeLimit() throws UnsupportedOperationException{
       return getActual().getSizeLimit();
    }
-   
+
    public double getWeightLimit() throws UnsupportedOperationException{
       return getActual().getWeightLimit();
    }
-   
+
    public double getContentsWeight() throws UnsupportedOperationException{
       return getActual().getContentsWeight();
    }
-   
+
    public double getUsedSpace() throws UnsupportedOperationException{
       return getActual().getUsedSpace();
    }
